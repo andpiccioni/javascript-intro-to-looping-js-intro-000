07413f1ae4b44557151825c15ad89bcc9db7d6e7
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-12 11:55:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,7 +16,7 @@ function whileLoop(n){
 
 
 function doWhileLoop(array){
-  var myArray =["ciao", "bye", "hello", "Help"];
+  var myArray =["ciao", "bye"];
   function maybeTrue() {
     return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
   };
